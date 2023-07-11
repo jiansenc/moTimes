b1eb1d143ebfc1491370b5ad415e694439f05b6c
jiansen <1096129293@qq.com>
2023-07-11 03:32:28 +0000

    ASS

--- work.js
+++ work.js
@@ -1,14 +1,4 @@
 const maxlen = 10000
-function insert(document) {
-    const list = document.querySelector('#list')
-    for (var i = 1; i <= maxlen; i++) {
-        let dom = document.createElement('div')
-        dom.innerHTML = `<span>这是第 ${i} 条数据</span>`
-        list.append(dom)
-        // prdom.innerHTML = '进度' + (i / maxlen * 100) + '%'
-    }
+for (var i = 1; i <= maxlen; i++) {
+    postMessage('渲染第'+ i + '个');
 }
-
-self.addEventListener('message', function (e) {
-    insert(e.data)
-  }, false);
